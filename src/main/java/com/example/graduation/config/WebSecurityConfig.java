9d443724dc122043834787e36e66a3eaf2e5bee6
VacuouS <vacuous21@gmail.com>
2023-05-26 23:59:30 +0300

    Убрал sequrity

--- src/main/java/com/example/graduation/config/WebSecurityConfig.java
+++ src/main/java/com/example/graduation/config/WebSecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.graduation.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
