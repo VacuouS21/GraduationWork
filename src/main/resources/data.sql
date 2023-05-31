575b6ced5560bc9f8e037cd55beed154be80f363
VacuouS <vacuous21@gmail.com>
2023-06-01 00:15:39 +0300

    База

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO teacher VALUES (0,'Anna','anna21','123123');
 INSERT INTO teacher VALUES (1,'Elza','Elza21','123123');
 INSERT INTO teacher VALUES (2,'Boris','Boris21','123123');
 
-INSERT INTO users VALUES (0,TRUE,0,0,0,1,'admin','123123',2);
-INSERT INTO users VALUES (1,TRUE,0,0,0,1,'operator','123123',1);
-INSERT INTO users VALUES (2,TRUE,0,0,0,1,'controller','123123',0);
+INSERT INTO users VALUES (0,TRUE,0,0,0,0,'admin','123123',2);
+INSERT INTO users VALUES (1,TRUE,0,0,0,0,'operator','123123',1);
+INSERT INTO users VALUES (2,TRUE,0,0,0,0,'controller','123123',0);
